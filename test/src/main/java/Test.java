947655a3b8b47bb5af887f489f1d19d08dc35757
xiayang <1123695790@qq.com>
2017-08-31 15:23:41 +0800

    首次提交。

---
+++ test/src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright © 2017 北京易酒批电子商务有限公司. All rights reserved.
+ */
+
+/**
+ *
+ * @author xiayang
+ * @date 2017/8/31
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.err.print("-----");
+    }
+}
